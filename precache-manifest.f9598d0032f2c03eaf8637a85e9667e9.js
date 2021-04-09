cb9e4380475e2e9e58c2cba3592fb55004162d9e
Anteneh Gebeyaw <antenehgeb@gmail.com>
2021-04-09 23:42:52 +0200

    new build

---
+++ precache-manifest.f9598d0032f2c03eaf8637a85e9667e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a530426846b5fa600bf490c1554e79e3",
+    "url": "/election/index.html"
+  },
+  {
+    "revision": "a2b7ce46f3d2827380d7",
+    "url": "/election/static/css/main.1ba81dc7.chunk.css"
+  },
+  {
+    "revision": "5ec59f3a01ebc5b00a24",
+    "url": "/election/static/js/2.6adcc1a7.chunk.js"
+  },
+  {
+    "revision": "cecf201d65141f661cec75bbf7e1920a",
+    "url": "/election/static/js/2.6adcc1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b7ce46f3d2827380d7",
+    "url": "/election/static/js/main.78f41ff2.chunk.js"
+  },
+  {
+    "revision": "b61cc3f162c19f5f97e3",
+    "url": "/election/static/js/runtime-main.e1f812d7.js"
+  }
+]);
